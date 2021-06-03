5bc0ce34d94eb0de642c84723940295bce23aae2
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2021-06-02 23:44:42 -0700

    simple quiz app with question bank and handle function to rotate thru questions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const questions = [
+		{
+			questionText: 'Who is the strongest avenger??',
+			answerOptions: [
+				{ answerText: 'Captain America', isCorrect: false },
+				{ answerText: 'Hulk', isCorrect: true },
+				{ answerText: 'Thor', isCorrect: false },
+				{ answerText: 'Captain Marvel', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is the most skilled fighter of the Avengers?',
+			answerOptions: [
+				{ answerText: 'Black Widow', isCorrect: true },
+				{ answerText: 'Hawkeye', isCorrect: false },
+				{ answerText: 'Captain America', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is the King of Wakanda?',
+			answerOptions: [
+				{ answerText: 'TChalla', isCorrect: true },
+				{ answerText: 'Hawkeye', isCorrect: false },
+				{ answerText: 'Shuri', isCorrect: false },
+				{ answerText: 'Nick Fury', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is the tactical leader of the Avengers?',
+			answerOptions: [
+				{ answerText: 'Iron Man', isCorrect: false },
+				{ answerText: 'Spider man', isCorrect: false },
+				{ answerText: 'Doctor Strange', isCorrect: false },
+				{ answerText: 'Captain America', isCorrect: true },
+			],
+		},
+	];
+
+  //variables need for question count, user score and correct answers 
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [showScore, setShowScore] = useState(false);
+
+  const [score, setScore] = useState(0);
+
+ //conditionals 
+ const handleAnsBtnClick = (isCorrect) => {
+   if(isCorrect){
+     setScore(score + 1)
+   }
+
+   const nextQuestion = currentQuestion +1;
+   if(nextQuestion < questions.length) {
+     setCurrentQuestion(nextQuestion);
+   }else{
+     setShowScore(true);
+   }
+ };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="app">
+     { showScore ? (<div className='score-section'> You scored {score} out of {questions.length}</div>) : (
+      <div> 
+        <div className="question-section">
+          <div className="question-count">
+            <span>Question {currentQuestion}</span>/{questions.length}
+          </div>
+            <div className='question-text'>
+              {questions[currentQuestion].questionText}
+            </div>
+            <div className='answer-section'>
+              {questions[currentQuestion].answerOptions.map((answerOption, index) => (<button onClick={() => handleAnsBtnClick(answerOption.isCorrect)}>{answerOption.answerText}</button>))}
+            </div>
+        </div>
+      </div>
+     )}
     </div>
   );
 }
